22b08a144b3a8635da4ddf4c472cf0db2dd060e9
Tin Nguyen <powertinnguyen@gmail.com>
2026-09-15 21:42:07 +0200

    #3 First HTML
    Alle content toegevoegd die nodig is voor de profile card in HTML.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import tin from "$lib/assets/tin.png"
+    import instagram from "$lib/assets/instagram.webp"
+    import github from "$lib/assets/github.webp"
+    import linkedin from "$lib/assets/linkedin.png"
+</script>
+
+<main>
+    <h1>
+        Profile Card
+    </h1>
+
+    <section>
+        <h2>Tin Nguyen</h2>
+        <h3>Student of Hogeschool van Amsterdam</h3>
+        <div>
+            <img src={tin} alt="tin" width="200">
+
+            <ul>
+                <li>
+                    <a href="https://www.instagram.com/tin_ng2004/">
+                        <img src={instagram} alt="instagram icon" width="35">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/WebTins?tab=repositories">
+                        <img src={github} alt="github icon" width="35">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/tin-nguyen-33a211202/">
+                        <img src={linkedin} alt="linkedin icon" width="35">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+</main>
+
+<footer>
+    <ul>
+        <li>@Almere</li>
+        <li>22yrs</li>
+        <li>Front-end</li>
+    </ul>
+</footer>
+
