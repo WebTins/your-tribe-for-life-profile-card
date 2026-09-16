a86d59ee64973f79a59c1b206c87cf09ed4ea0b4
Tin Nguyen <powertinnguyen@gmail.com>
2026-09-16 23:26:57 +0200

    #6 Stylesheet setup
    Ik heb een begin van de stylesheet erin gezet en een font toegevoegd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+	import '../../styles/style.css'
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+  	<title>Tin Nguyen</title>
 </svelte:head>
 
 {@render children()}
